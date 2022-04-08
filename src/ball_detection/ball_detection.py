c673d0de25a6ddabfbbf271bd4f79911841dd73e
snshen <snshen@andrew.cmu.edu>
2022-04-08 18:37:00 -0400

    updated ball detection for ros

--- src/ball_detection/ball_detection.py
+++ src/ball_detection/ball_detection.py
@@ -1,3 +1,5 @@
+import rospy
+from geometry_msgs.msg import PointStamped
 from collections import deque
 from imutils.video import VideoStream
 import numpy as np
@@ -6,26 +8,42 @@ import cv2
 import imutils
 import time
 
-def detectBall(visualize = True):
-    # define the lower and upper boundaries of the "green" ball in the HSV color space
-    greenLower = (29, 86, 6)
-    greenUpper = (64, 255, 255)
-    # grab the reference to the webcam
-    vs = VideoStream(src=0).start()
-    # if we want to threshold out smaller contours
-    rad_thresh = 0
+class BallDetector:
 
-    # allow the camera or video file to warm up
-    time.sleep(2.0)
+    def __init__(self) -> None:
 
-    # keep looping
-    while True:
-        # grab the current frame
-        frame = vs.read()
+        self.ball_pos_msg = PointStamped()
+
+        self.P = np.array([[1, 0, 0, 0],
+                            [0, 1, 0, 0],
+                            [0, 0, 1, 0]])
+
+        self.sub = rospy.Subscriber(
+        "/kinect_frame", # TODO: check topic 
+        PointStamped, 
+        self.detectBall)
+
+        self.pub = rospy.Publisher(
+        "/ball_detections", 
+        PointStamped,
+        queue_size=2)
+
+
+    def detectBall(self, msg):
+        # define the lower and upper boundaries of the "green" ball in the HSV color space
+        greenLower = (29, 86, 6)
+        greenUpper = (64, 255, 255)
+        
+        # current rgb frame
+        rgbframe = msg.rgbmap # TODO: check msg 
+        dframe = msg.dmap # TODO: check msg 
+
+        # if we want to threshold out smaller contours
+        rad_thresh = 0
 
         # resize the frame, blur it, and convert it to the HSVcolor space
-        frame = imutils.resize(frame, width=600)
-        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        rgbframe = imutils.resize(rgbframe, width=600)
+        blurred = cv2.GaussianBlur(rgbframe, (11, 11), 0)
         hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
         # construct a mask for the color "green"
@@ -35,36 +53,37 @@ def detectBall(visualize = True):
         mask = cv2.dilate(mask, None, iterations=2)
 
         # find contours in the mask and initialize the current (x, y) center of the ball
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
-        center = None
         # only proceed if at least one contour was found
         if len(cnts) > 0:
             # find the largest contour in the mask
             c = max(cnts, key=cv2.contourArea)
             # compute the minimum enclosing circle and centroid
             ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            z = dframe[int(x)][int(y)]
+
             # only proceed if the radius meets a minimum size
-            if radius > rad_thresh: # tuning param
-                # draw the circle and centroid on the frame,
-                # then update the list of tracked points
-                cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
-                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            # if radius > rad_thresh: # tuning param
 
-        if visualize:
-            # show the frame to our screen
-            cv2.imshow("Frame", frame)
-        
-        key = cv2.waitKey(1) & 0xFF
-        # if the 'q' key is pressed, stop the loop
-        if key == ord("q"):
-            break
+            #TODO: get extrinsic calibration
+            X = np.array([x, y, z, 1]).T
+            ball_X = self.P@X
 
-    vs.stop()
-    cv2.destroyAllWindows()
+            ball_x = ball_X[0]
+            ball_y = ball_X[1]
+            ball_z = ball_X[2]
+
+            # Set up message
+            self.ball_pos_msg.header = msg.header
+            self.ball_pos_msg.point.x = ball_x
+            self.ball_pos_msg.point.y = ball_y
+            self.ball_pos_msg.point.z = ball_z
+
+            self.pub.publish(self.intersect_pos_msg)
+            
 
 if __name__ == '__main__':
-    detectBall()
+    rospy.init_node('ball_detector')
+    BallDetector()
+    rospy.spin()
