88b868f27062e6312fb319e078b7bb87b07ec858
snshen <snshen@andrew.cmu.edu>
2022-04-08 22:36:26 -0400

    updated kinect msgs

--- src/ball_detection/ball_detection.py
+++ src/ball_detection/ball_detection.py
@@ -7,42 +7,54 @@ import argparse
 import cv2
 import imutils
 import time
+from autolab_core import RigidTransform, Point
+from perception import CameraIntrinsics
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 class BallDetector:
 
     def __init__(self) -> None:
 
         self.ball_pos_msg = PointStamped()
+        self.intrinsics = azure_kinect_intrinsics = CameraIntrinsics.load('config/azure_kinect.intr')
+        self.extrinsics = azure_kinect_to_world_transform = RigidTransform.load('config/azure_kinect_overhead/azure_kinect_overhead_to_world.tf')
+        self.rgb_map = None
+        self.depth_map = None
 
-        self.P = np.array([[1, 0, 0, 0],
-                            [0, 1, 0, 0],
-                            [0, 0, 1, 0]])
+        self.bridge = CvBridge()
 
         self.sub = rospy.Subscriber(
-        "/kinect_frame", # TODO: check topic 
+        "/rgb_to_depth",
         PointStamped, 
-        self.detectBall)
+        self.depth_clbk)
+
+        self.sub = rospy.Subscriber(
+        "/depth_to_rgb",
+        PointStamped, 
+        self.rgb_clbk)
 
         self.pub = rospy.Publisher(
         "/ball_detections", 
         PointStamped,
         queue_size=2)
 
+    def rgb_clbk(self, msg):
+        self.rgb_map  = self.bridge.imgmsg_to_cv2(msg.raw_image.data, msg.encoding)
+    
+    def depth_clbk(self, msg):
+        self.depth_map  = self.bridge.imgmsg_to_cv2(msg.raw_image.data, msg.encoding)
 
     def detectBall(self, msg):
         # define the lower and upper boundaries of the "green" ball in the HSV color space
         greenLower = (29, 86, 6)
         greenUpper = (64, 255, 255)
-        
-        # current rgb frame
-        rgbframe = msg.rgbmap # TODO: check msg 
-        dframe = msg.dmap # TODO: check msg 
 
         # if we want to threshold out smaller contours
         rad_thresh = 0
 
         # resize the frame, blur it, and convert it to the HSVcolor space
-        rgbframe = imutils.resize(rgbframe, width=600)
+        rgbframe = imutils.resize(self.rgb_map, width=600)
         blurred = cv2.GaussianBlur(rgbframe, (11, 11), 0)
         hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
@@ -56,32 +68,45 @@ class BallDetector:
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
         # only proceed if at least one contour was found
-        if len(cnts) > 0:
+        if len(cnts) > 0 and self.rgb_map is not None and self.depth_map is not None:
             # find the largest contour in the mask
             c = max(cnts, key=cv2.contourArea)
             # compute the minimum enclosing circle and centroid
             ((x, y), radius) = cv2.minEnclosingCircle(c)
-            z = dframe[int(x)][int(y)]
 
             # only proceed if the radius meets a minimum size
             # if radius > rad_thresh: # tuning param
 
             #TODO: get extrinsic calibration
-            X = np.array([x, y, z, 1]).T
-            ball_X = self.P@X
+            ball_center = self.get_object_center_point_in_world(x, y, self.depth_map)
 
-            ball_x = ball_X[0]
-            ball_y = ball_X[1]
-            ball_z = ball_X[2]
+
+            # X = np.array([x, y, 1]).T
+            # ball_X = self.P@X
+
+            # ball_x = ball_X[0]/ball_X[2]
+            # ball_y = ball_X[1]/ball_X[2]
+            # ball_z = dframe[int(x)][int(y)]
 
             # Set up message
             self.ball_pos_msg.header = msg.header
-            self.ball_pos_msg.point.x = ball_x
-            self.ball_pos_msg.point.y = ball_y
-            self.ball_pos_msg.point.z = ball_z
+            self.ball_pos_msg.point.x = ball_center[0]
+            self.ball_pos_msg.point.y = ball_center[1]
+            self.ball_pos_msg.point.z = ball_center[2]
 
             self.pub.publish(self.intersect_pos_msg)
-            
+
+    def get_point_in_world(self, object_image_center_x, object_image_center_y, depth_image): 
+        
+        object_center = Point(np.array([object_image_center_x, object_image_center_y]), 'azure_kinect_overhead')
+        object_depth = depth_image[object_image_center_y, object_image_center_x]
+        # print("x, y, z: ({:.4f}, {:.4f}, {:.4f})".format(
+        #     object_image_center_x, object_image_center_y, object_depth))
+        object_center_point_in_world = self.extrinsics * self.intrinsics.deproject_pixel(object_depth, object_center)    
+        # print(object_center_point_in_world)
+
+        return object_center_point_in_world
+                
 
 if __name__ == '__main__':
     rospy.init_node('ball_detector')
